e5175d398b4c1a4163599ef6d88c7561cd70e230
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-28 10:05:55 +0600

    Display all meal as card using grid layout on the meals page

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 // fetch all meals
 export const fetchMeals = async () => {
     try{
@@ -23,9 +26,17 @@ const Meals = async() => {
                 <div className={`mt-8 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4`}>
                     {
                         meals?.map((meal, index) => {
+                            {/* console.log("Single meal:", meal); */}
+
                             return (
-                                <div className={`border border-gray-300 rounded p-2 hover:shadow-md hover:cursor-pointer`}>
-                                    <p>Single meal</p>
+                                <div key={meal?.idMeal} className={`border border-gray-300 rounded p-2 hover:shadow-md hover:cursor-pointer`}>
+                                    <Image src={meal?.strMealThumb} width={150} height={175} alt={meal?.strMeal} className={`w-full h-[125px] rounded`} />
+
+                                    <div className={`mt-3`}>
+                                        <h1 className={`text-lg text-slate-700 font-medium`}>{meal?.strMeal}</h1>
+                                        <p className={`text-sm text-gray-500 my-1`}>{meal?.strInstructions.slice(0, 150)}...</p>
+                                        <Link href={`/meals/${meal?.idMeal}`}><button className={`px-5 py-1 text-sm text-slate-700 border border-gray-300 rounded outline-none hover:bg-gray-100 active:bg-transparent`}>Show details</button></Link>
+                                    </div>
                                 </div>
                             )
                         })
