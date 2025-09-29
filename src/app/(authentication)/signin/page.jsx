42179a35a618b2fb93f8e69da8e0fbfbe017674d
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-29 18:27:39 +0600

    Signin & Signup page updated

--- src/app/(authentication)/signin/page.jsx
+++ src/app/(authentication)/signin/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SignInForm from "./components/SignInForm";
 
 const SignIn = () => {
@@ -6,8 +7,12 @@ const SignIn = () => {
             <div className="container mx-auto w-full px-6 lg:px-8 py-8">
                 <h1 className='text-center text-2xl text-slate-700 font-medium'>Sign in page</h1>
 
-                <div className="mt-8">
+                <div className="my-8">
                     <SignInForm />
+                </div>
+
+                <div className="text-center">
+                    <h1 className="text-sm text-gray-500">Dont have an account? <Link href={`/signup`} className="text-blue-500 hover:text-blue-400 active:text-blue-500 font-medium">Sign up</Link></h1>
                 </div>
             </div>
         </div>
--- src/app/(authentication)/signup/page.jsx
+++ src/app/(authentication)/signup/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SignUpForm from "./components/SignUpForm";
 
 const SignUp = () => {
@@ -6,8 +7,12 @@ const SignUp = () => {
             <div className="container mx-auto w-full px-6 lg:px-8 py-8">
                 <h1 className='text-center text-2xl text-slate-700 font-medium'>Sign up page</h1>
 
-                <div className="mt-8">
+                <div className="my-8">
                     <SignUpForm />
+                </div>
+
+                <div className="text-center">
+                    <h1 className="text-sm text-gray-500">Already have an account? <Link href={`/signin`} className="text-blue-500 hover:text-blue-400 active:text-blue-500 font-medium">Sign in</Link></h1>
                 </div>
             </div>
         </div>
