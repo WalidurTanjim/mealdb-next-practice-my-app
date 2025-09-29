f2eb0ca4860a869d79d750351a0b139902c6cef4
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-29 13:23:27 +0600

    put request working well

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -9,3 +9,22 @@ export const DELETE = async(request, { params }) => {
 
     return Response.json({ result });
 }
+
+
+export const PUT = async(request, { params }) => {
+    const data = await request.json();
+    
+    const { id } = await params;
+    const query = { _id: new ObjectId(id) };
+    const testUserCollection = await dbConnect(collectionsNames.TEST_USER);
+    const user = await testUserCollection.findOne(query);
+    if(user){
+        const updatedUser = {
+            $set: { ...data }
+        };
+        const result = await testUserCollection.updateOne(query, updatedUser);
+        return Response.json({ result });
+    }
+
+    return Response.json({ message: "Something went wrong! Please try again." })
+}
