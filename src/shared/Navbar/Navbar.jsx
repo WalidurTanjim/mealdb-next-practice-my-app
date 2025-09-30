b60be2dc2ef66869219ba60585d79477576b3114
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-30 20:19:42 +0600

    loggedin user photo will show on the navbar

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useSession } from 'next-auth/react';
 import Link from "next/link";
 import { signOut } from "next-auth/react"
+import Image from 'next/image';
 
 const navigation = [
     { name: 'Home', href: '/', current: false },
@@ -80,11 +81,11 @@ const Navbar = () => {
                                     <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                                         <span className="absolute -inset-1.5" />
                                         <span className="sr-only">Open user menu</span>
-                                        <img
-                                            alt=""
-                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
-                                        />
+                                        {
+                                            sessionData?.user?.image ? 
+                                            <Image src={sessionData?.user?.image} alt={sessionData?.user?.name} width={40} height={40} className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10" /> :
+                                            <img alt="" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10" />
+                                        }
                                     </MenuButton>
 
                                     <MenuItems
